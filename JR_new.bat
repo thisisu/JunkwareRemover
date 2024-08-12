dc131e4daadb6a0d9ec81b772e88a00aeb6cad67
d1savow3d <35790653+thisisu@users.noreply.github.com>
2024-08-11 21:07:35 -0500

    mini revamp

--- JR_new.bat
+++ JR_new.bat
@@ -1,7 +1,7 @@
 :: JunkwareRemover
-:: Created by Thisisu
+:: Created by Furtivex
 @echo OFF
-title JunkwareRemover - Version 1.0.1
+title JunkwareRemover - Version 1.0.2
 REM ~~~~~~~~~~~~~~~~~~~~~~~~>
 SET "jr=%TEMP%\jr"
 SET "JRTEMP=%TEMP%\jr\TEMP"
@@ -27,61 +27,83 @@ if %ARCH%==x64 (
 REM ~~~~~~~~~~~~~~~~~~~~~~~~>
 FOR /F "tokens=2*" %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName" /v ComputerName 2^>NUL') DO SET COMPUTERNAME=%%B
 FOR /F "tokens=2*" %%A IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>NUL') DO SET OS=%%B
-ECHO %OS%|FIND "Windows XP" >NUL
-IF %ERRORLEVEL% EQU 0 (
- SET "LOCALA=%userprofile%\Local Settings\Application Data"
- SET "MYDOCS=%userprofile%\My Documents"
- SET "MYDLS=%userprofile%\My Documents\Downloads"
- SET "MYMUSIC=%userprofile%\My Documents\My Music"
- SET "LNK1XP=%allusersprofile%\Start Menu"
- SET "LNK2XP=%userprofile%\Start Menu"
- SET "STARTUP=%userprofile%\Start Menu\Programs\Startup"
- SET "DRM=%allusersprofile%\DRM"
- SET "TIFS=%systemdrive%\Documents and Settings\LocalService\Local Settings\Temporary Internet Files\Content.IE5"
- SET "TIFS2=%windir%\System32\config\systemprofile\Local Settings\Temporary Internet Files\Content.IE5"
-) ELSE (
- SET "LOCALA=%localappdata%"
- SET "LOCALLOW=%userprofile%\Appdata\LocalLow"
- SET "MYDOCS=%userprofile%\Documents"
- SET "MYDLS=%userprofile%\Downloads"
- SET "MYMUSIC=%userprofile%\Music"
- SET "PUBDESKTOP=%systemdrive%\Users\Public\Desktop"
- SET "PUBDOCS=%systemdrive%\users\Public\Documents"
- SET "PUBLIC=%systemdrive%\Users\Public"
- SET "QUICKLAUNCH17=%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu"
- SET "QUICKLAUNCH27=%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
- SET "PROGRAMS17=%allusersprofile%\Microsoft\Windows\Start Menu\Programs"
- SET "PROGRAMS27=%appdata%\Microsoft\Windows\Start Menu\Programs"
- SET "STARTMENU17=%allusersprofile%\Microsoft\windows\Start Menu"
- SET "STARTMENU27=%appdata%\Microsoft\Windows\Start Menu"
- SET "STARTUP=%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
- SET "DRM=%allusersprofile%\Microsoft\DRM"
- SET "TIFS=%localappdata%\Microsoft\Windows\Temporary Internet Files\Content.IE5"
- SET "TIFS2=%windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5"
+SET "LOCALA=%localappdata%"
+SET "LOCALLOW=%userprofile%\Appdata\LocalLow"
+SET "MYDLS=%userprofile%\Downloads"
+SET "MYDOCS=%userprofile%\Documents"
+SET "MYMUSIC=%userprofile%\Music"
+SET "PROGRAMS17=%allusersprofile%\Microsoft\Windows\Start Menu\Programs"
+SET "PROGRAMS27=%appdata%\Microsoft\Windows\Start Menu\Programs"
+SET "PUBDESKTOP=%systemdrive%\Users\Public\Desktop"
+SET "PUBDOCS=%systemdrive%\users\Public\Documents"
+SET "PUBLIC=%systemdrive%\Users\Public"
+SET "QUICKLAUNCH17=%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu"
+SET "QUICKLAUNCH27=%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+SET "STARTMENU17=%allusersprofile%\Microsoft\windows\Start Menu"
+SET "STARTMENU27=%appdata%\Microsoft\Windows\Start Menu"
+SET "STARTUP=%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+SET "TIFS2=%windir%\System32\config\systemprofile\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5"
+SET "TIFS=%localappdata%\Microsoft\Windows\Temporary Internet Files\Content.IE5"
+
+
+
+:: Processes
+
+TASKKILL /F /IM "msedge.exe"
+
+:: Tasks
+for %%i in (
+"MicrosoftEdgeUpdateTaskMachineCore"
+"MicrosoftEdgeUpdateTaskMachineUA"
+"Microsoft\Windows\Maintenance\WinSAT"
+"Microsoft\XblGameSave"
+"Microsoft\XblGameSave\XblGameSaveTask"
+) DO (
+       SCHTASKS /DELETE /TN %%i /F
+      )
 )
 
-:: WarcraftLogs
-DEL /F/Q "%PROGRAMFILES(x86)%\World of Warcraft\_retail_\Logs\RaiderIOLogsArchive\*" >NUL 2>&1
-DEL /F/Q "%PROGRAMFILES(x86)%\World of Warcraft\_xptr_\Logs\*" >NUL 2>&1
-DEL /F/Q "%PROGRAMFILES(x86)%\World of Warcraft\_retail_\Logs\WoWCombat*.txt" >NUL 2>&1
+:: Services
 
-:: Microsoft Edge Uninstaller
-TASKKILL /F /IM "msedge.exe" >NUL 2>&1
-REG DELETE "HKLM\SYSTEM\CurrentControlSet\services\edgeupdate" /F >NUL 2>&1
-REG DELETE "HKLM\SYSTEM\CurrentControlSet\services\edgeupdatem" /F >NUL 2>&1
-REG DELETE "HKLM\SYSTEM\CurrentControlSet\services\MicrosoftEdgeElevationService" /F >NUL 2>&1
+for %%i in (
+"HKLM\SYSTEM\CurrentControlSet\services\edgeupdate"
+"HKLM\SYSTEM\CurrentControlSet\services\edgeupdatem"
+"HKLM\SYSTEM\CurrentControlSet\services\MicrosoftEdgeElevationService"
+) DO (
+       REG DELETE %%i /F
+      )
+)
 
-RD /S/Q "%PROGRAMFILES(x86)%\Microsoft\Edge" >NUL 2>&1
-RD /S/Q "%PROGRAMFILES(x86)%\Microsoft\EdgeCore" >NUL 2>&1
-RD /S/Q "%PROGRAMFILES(x86)%\Microsoft\EdgeUpdate" >NUL 2>&1
-RD /S/Q "%userprofile%\MicrosoftEdgeBackups" >NUL 2>&1
-RD /S/Q "%LOCALLOW%\Microsoft\Internet Explorer" >NUL 2>&1
+:: Files
 
-DEL /F/Q "%APPDATA%\discord\Cache\Cache_Data\*" >NUL 2>&1
-DEL /F/Q "%APPDATA%\discord\Code Cache\js\*" >NUL 2>&1
-DEL /F/Q "%USERPROFILE%\Desktop\Microsoft Edge.lnk" >NUL 2>&1
-DEL /F/Q "%PROGRAMS27%\Microsoft Edge.lnk" >NUL 2>&1
-DEL /F/Q "%userprofile%\Favorites\Bing.url" >NUL 2>&1
-DEL /F/Q "%QUICKLAUNCHALL%\*.lnk" >NUL 2>&1
-DEL /F/Q/S "%WINDIR%\Logs\*.etl" >NUL 2>&1
-DEL /F/Q/S "%WINDIR%\Logs\*.log" >NUL 2>&1
+for %%i in (
+"%PROGRAMFILES(x86)%\World of Warcraft\_retail_\Logs\RaiderIOLogsArchive\*"
+"%PROGRAMFILES(x86)%\World of Warcraft\_xptr_\Logs\*"
+"%PROGRAMFILES(x86)%\World of Warcraft\_retail_\Logs\WoWCombat*.txt"
+"%APPDATA%\discord\Cache\Cache_Data\*"
+"%APPDATA%\discord\Code Cache\js\*"
+"%USERPROFILE%\Desktop\Microsoft Edge.lnk"
+"%PROGRAMS27%\Microsoft Edge.lnk"
+"%USERPROFILE%\Favorites\Bing.url"
+"%QUICKLAUNCHALL%\*.lnk"
+) DO (
+      DEL /F/Q %%i
+     )
+)
+
+:: Folders
+for %%i in (
+"%APPDATA%\Microsoft\Internet Explorer
+"%LOCALA%\Microsoft\Edge"
+"%LOCALA%\Microsoft\XboxLive"
+"%LOCALLOW%\Microsoft\Internet Explorer"
+"%PROGRAMFILES%\Microsoft\EdgeUpdater"
+"%PROGRAMFILES(x86)%\Microsoft\Edge"
+"%PROGRAMFILES(x86)%\Microsoft\EdgeCore"
+"%PROGRAMFILES(x86)%\Microsoft\EdgeUpdate"
+"%USERPROFILE%\MicrosoftEdgeBackups"
+) DO (
+      IF EXIST %%i (
+                     RD /S/Q %%i
+                    )
+)
